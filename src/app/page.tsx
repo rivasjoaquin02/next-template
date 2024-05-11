a2f1f1d4375064e2f915f4a726ddc0ec2f8efcf8
Strange <rivasjoaquin02@gmail.com>
2024-05-11 12:07:18 -0400

    refactor: add prettier config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Form from '@/components/form';
 
 const Root = () => {
-	return (
-		<main className='flex items-center justify-center h-screen w-screen'>
-			<Form />
-		</main>
-	);
+    return (
+        <main className='flex items-center justify-center h-screen w-screen'>
+            <Form />
+        </main>
+    );
 };
 
 export default Root;
